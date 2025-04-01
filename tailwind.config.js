7298e4f2c0a5094502cd0f7c27feee480ebf8d03
lalit bafna <labafna@gmail.com>
2025-04-01 14:44:06 +0530

    Initial commit with Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./layout/**/*.liquid", "./templates/**/*.liquid", "./sections/**/*.liquid"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
